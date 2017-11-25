32ab7ea300355fcef37bfb7989252629f9b271a7
jimposibul <jimposibul@gmail.com>
2017-11-25 23:33:51 +0100

    Template for future interface

--- src/main/java/com/project/common/Controller.java
+++ src/main/java/com/project/common/Controller.java
@@ -1,5 +1,6 @@
 package com.project.common;
 import com.project.model.Car;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -7,8 +8,15 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
 
 import java.net.URL;
+import java.util.Iterator;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
@@ -27,7 +35,21 @@ public class Controller implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        System.out.println("View is now loaded!");
+        col1.setCellValueFactory(new PropertyValueFactory<Car,String>("brand"));
+        col2.setCellValueFactory(new PropertyValueFactory<Car,String>("model"));
+
+        data  =  FXCollections.observableArrayList();
+        SessionFactory sf = new Configuration().configure().buildSessionFactory();
+        Session sess  =sf.openSession();
+        Query qee = sess.createQuery("from Car ");
+        Iterator ite  =qee.iterate();
+        while(ite.hasNext())
+        {
+            Car obj = (Car)ite.next();
+
+            data.add(obj);
+        }
+        table.setItems(data);
     }
 
 
