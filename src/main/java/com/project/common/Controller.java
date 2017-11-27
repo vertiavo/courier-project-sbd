91614a0f32dce562c24468b935d9c3eaa5771ada
Marek Jakubowski <vertiavo@gmail.com>
2017-11-27 19:54:45 +0100

    added listView to view and its controller

--- src/main/java/com/project/common/Controller.java
+++ src/main/java/com/project/common/Controller.java
@@ -1,56 +1,137 @@
 package com.project.common;
+
+import com.project.dao.AreaDao;
+import com.project.dao.CarDao;
+import com.project.dao.CourierCarAreaDao;
+import com.project.dao.CourierDao;
+import com.project.dao.OfferDao;
+import com.project.dao.OrderInfoDao;
+import com.project.dao.PackageDimensionDao;
+import com.project.dao.PackageInfoDao;
+import com.project.dao.PaymentDao;
+import com.project.dao.RecipientDao;
+import com.project.dao.SenderDao;
+import com.project.dao.jpa.AreaJpaDao;
+import com.project.dao.jpa.CarJpaDao;
+import com.project.dao.jpa.CourierCarAreaJpaDao;
+import com.project.dao.jpa.CourierJpaDao;
+import com.project.dao.jpa.OfferJpaDao;
+import com.project.dao.jpa.OrderInfoJpaDao;
+import com.project.dao.jpa.PackageDimensionJpaDao;
+import com.project.dao.jpa.PackageInfoJpaDao;
+import com.project.dao.jpa.PaymentJpaDao;
+import com.project.dao.jpa.RecipientJpaDao;
+import com.project.dao.jpa.SenderJpaDao;
 import com.project.model.Car;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
-import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.query.Query;
-
 
 import java.net.URL;
-import java.util.Iterator;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+
+    private static final String AREA = "Area";
+    private static final String CAR = "Car";
+    private static final String COURIER = "Courier";
+    private static final String COURIER_CAR_AREA = "CourierCarArea";
+    private static final String OFFER = "Offer";
+    private static final String ORDER_INFO = "OrderInfo";
+    private static final String PACKAGE_DIMENSION = "PackageDimension";
+    private static final String PACKAGE_INFO = "PackageInfo";
+    private static final String PAYMENT = "Payment";
+    private static final String RECIPIENT = "Recipient";
+    private static final String SENDER = "Sender";
 
     @FXML
     private TableView<Car> table;
 
     @FXML
-    private TableColumn<Car, String> col1; //brand
+    private ListView<String> listView;
 
-    @FXML
-    private TableColumn<Car, String> col2; //model
-
-    public ObservableList<Car> data;
-
+    private ObservableList<Car> data;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        col1.setCellValueFactory(new PropertyValueFactory<Car,String>("brand"));
-        col2.setCellValueFactory(new PropertyValueFactory<Car,String>("model"));
-
         data  =  FXCollections.observableArrayList();
-        SessionFactory sf = new Configuration().configure().buildSessionFactory();
-        Session sess  =sf.openSession();
-        Query qee = sess.createQuery("from Car ");
-        Iterator ite  =qee.iterate();
-        while(ite.hasNext())
-        {
-            Car obj = (Car)ite.next();
 
-            data.add(obj);
-        }
+        setListItems();
+        setListClickEvent();
+
         table.setItems(data);
     }
 
+    private void setListItems() {
+        listView.setItems(FXCollections.observableArrayList(
+                AREA,
+                CAR,
+                COURIER,
+                COURIER_CAR_AREA,
+                OFFER,
+                ORDER_INFO,
+                PACKAGE_DIMENSION,
+                PACKAGE_INFO,
+                PAYMENT,
+                RECIPIENT,
+                SENDER
+        ));
+    }
+
+    private void setListClickEvent() {
+        listView.setOnMouseClicked(e -> {
+            String selectedItem = listView.getSelectionModel().getSelectedItem();
+            switch (selectedItem) {
+                case AREA:
+                    AreaDao areaDao = new AreaJpaDao();
+                    System.out.println(areaDao.getAll());
+                    break;
+                case CAR:
+                    CarDao carDao = new CarJpaDao();
+                    System.out.println(carDao.getAll());
+                    break;
+                case COURIER:
+                    CourierDao courierDao = new CourierJpaDao();
+                    System.out.println(courierDao.getAll());
+                    break;
+                case COURIER_CAR_AREA:
+                    CourierCarAreaDao courierCarAreaDao = new CourierCarAreaJpaDao();
+                    System.out.println(courierCarAreaDao.getAll());
+                    break;
+                case OFFER:
+                    OfferDao offerDao = new OfferJpaDao();
+                    System.out.println(offerDao.getAll());
+                    break;
+                case ORDER_INFO:
+                    OrderInfoDao orderInfoDao = new OrderInfoJpaDao();
+                    System.out.println(orderInfoDao.getAll());
+                    break;
+                case PACKAGE_DIMENSION:
+                    PackageDimensionDao packageDimensionDao = new PackageDimensionJpaDao();
+                    System.out.println(packageDimensionDao.getAll());
+                    break;
+                case PACKAGE_INFO:
+                    PackageInfoDao packageInfoDao = new PackageInfoJpaDao();
+                    System.out.println(packageInfoDao.getAll());
+                    break;
+                case PAYMENT:
+                    PaymentDao paymentDao = new PaymentJpaDao();
+                    System.out.println(paymentDao.getAll());
+                    break;
+                case RECIPIENT:
+                    RecipientDao recipientDao = new RecipientJpaDao();
+                    System.out.println(recipientDao.getAll());
+                    break;
+                case SENDER:
+                    SenderDao senderDao = new SenderJpaDao();
+                    System.out.println(senderDao.getAll());
+                    break;
+            }
+        });
+    }
 
 }
--- src/main/java/com/project/common/Main.java
+++ src/main/java/com/project/common/Main.java
@@ -1,95 +1,22 @@
 package com.project.common;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import com.project.dao.AreaDao;
-import com.project.dao.CarDao;
-import com.project.dao.CourierCarAreaDao;
-import com.project.dao.CourierDao;
-import com.project.dao.OfferDao;
-import com.project.dao.SenderDao;
-import com.project.dao.jpa.AreaJpaDao;
-import com.project.dao.jpa.CarJpaDao;
-import com.project.dao.jpa.CourierCarAreaJpaDao;
-import com.project.dao.jpa.CourierJpaDao;
-import com.project.dao.jpa.OfferJpaDao;
-import com.project.dao.jpa.SenderJpaDao;
-import com.project.model.Area;
-import com.project.model.Car;
-import com.project.model.Courier;
-import com.project.model.CourierCarArea;
-import com.project.model.CourierCarAreaId;
-import com.project.model.Offer;
-import com.project.model.Sender;
-
-import java.util.Date;
-
-import static javafx.application.Application.launch;
 
 public class Main extends Application{
 
     public static void main(final String[] args) {
-
         launch(args);
-
-
-        /*
-        // Creating APIs for data transfer
-        CarDao carDao = new CarJpaDao();
-        OfferDao offerDao = new OfferJpaDao();
-        SenderDao senderDao = new SenderJpaDao();
-        CourierDao courierDao = new CourierJpaDao();
-        AreaDao areaDao = new AreaJpaDao();
-        CourierCarAreaDao courierCarAreaDao = new CourierCarAreaJpaDao();
-//
-//        Car car1 = new Car();
-//        car1.setBrand("Fiat");
-//        car1.setModel("Multipla");
-//        car1.setLoad(50d);
-//        car1.setCapacity(15d);
-//        carDao.save(car1);
-
-        Car car2=new Car();
-        car2.setBrand("Audi");
-        car2.setModel("80");
-        car2.setLoad(50d);
-        car2.setCapacity(60d);
-
-        carDao.save(car2);
-
-        Offer offer1 = new Offer("STANDARD", 0);
-        offerDao.save(offer1);
-
-        Sender sender1 = new Sender();
-        sender1.setName("Zbigniew");
-        sender1.setSurname("Dwunoga");
-        sender1.setAddress("Wiejska");
-        sender1.setOfferType(offerDao.findById("STANDARD"));
-        senderDao.save(sender1);
-
-        Area area1 = new Area("BOJARY");
-        areaDao.save(area1);
-
-        Courier courier1 = new Courier("Jan", "Kowalski", "Piastowska", 123456789);
-        courierDao.save(courier1);
-
-        CourierCarArea courierCarArea1 = new CourierCarArea();
-        // Setting primary key with embeddable class (PKs is courier and begin date)
-        courierCarArea1.setIdCourierCarArea(new CourierCarAreaId(courier1, new Date()));
-        courierCarArea1.setIdCar(carDao.findById(1));
-        courierCarArea1.setIdArea(area1);
-        courierCarArea1.setEndDate(new Date());
-        courierCarAreaDao.save(courierCarArea1);
-        */
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Main.fxml"));
 
-        primaryStage.setTitle("FXML Welcome");
+        primaryStage.setTitle("Courier Project");
         primaryStage.setScene(new Scene(root,300,275));
         primaryStage.show();
     }
